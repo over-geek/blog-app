5cfc863f926119ee58dda3411caf440b89b5ee5d
kenagy100@gmail.com <kenagy100@gmail.com>
2023-09-30 12:31:02 +0000

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'user_id', class_name: 'Comment'
   has_many :likes, foreign_key: 'user_id', class_name: 'Like'
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+  after_initialize do |user|
+    user.posts_counter = 0
+  end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
