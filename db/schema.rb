4cf43a60f2f5b3d085f5803e7e3ad27d6b34107b
kenagy100@gmail.com <kenagy100@gmail.com>
2023-09-30 12:39:21 +0000

    Modify files

--- db/schema.rb
+++ db/schema.rb
@@ -53,9 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_175844) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts" column: "post_id"
-  add_foreign_key "comments", "users" column: "user_id"
-  add_foreign_key "likes", "posts" column: "post_id"
-  add_foreign_key "likes", "users" column: "user_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
