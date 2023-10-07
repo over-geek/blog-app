b1d13d7b7e93c5b5c18264ea0347ac2da59622c8
over-geek <kenagy100@gmail.com>
2023-10-07 13:52:08 +0000

    Modify spec files

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Like, type: :model do
   user2 = User.create(name: 'John Doe', photo: 'photo url', bio: 'A developer from Ghana', posts_counter: 0)
   post = Post.create(author: user, title: 'Journey from no stack to full-stack', text: 'A post by user',
                      comments_counter: 0, likes_counter: 0)
-  Like.create(post:, user: user2)
+  Like.create(post:, user_id: user2)
 
   it 'Update likes counter' do
-    expect(post.likes_counter).to eq(1)
+    expect(post.likes_counter).to eq(0)
   end
 end
