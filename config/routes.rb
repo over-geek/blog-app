dac95c4b2c83fbd1e534483e7ef72b283ab2bf98
over-geek <kenagy100@gmail.com>
2023-10-07 15:54:06 +0000

    Modify file

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,15 @@ Rails.application.routes.draw do
   # root "articles#index"
    root "users#index"
 
+  get 'posts/:post_id/comments/new', to: 'comments#new'
+post 'posts/:post_id/comments', to: 'comments#create'
+post 'posts/:post_id', to: 'likes#create'
+  post 'posts', to: 'posts#create'
+  
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+     resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create]
+    end
   end
 end
