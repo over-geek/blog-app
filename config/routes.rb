92668aa81fc47697e07e876b0018f2067a4a3806
over-geek <kenagy100@gmail.com>
2023-10-07 20:12:11 +0000

    Refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-   root "users#index"
-
-  get 'posts/:post_id/comments/new', to: 'comments#new'
-post 'posts/:post_id/comments', to: 'comments#create'
-post 'posts/:post_id', to: 'likes#create'
-  post 'posts', to: 'posts#create'
-  
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
-     resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:create]
+  # Adding nested resources routes for users and posts
+  resources :users , only: [:index, :show] do
+    resources :posts, only: [:index, :new, :create, :show] do
+      post 'addlike', on: :member
+      delete 'deletelike', on: :member
+      resources :comments, only: [:new, :create]
     end
   end
+
+  # Defines the root path route ("/")
+  root "users#index"
 end
