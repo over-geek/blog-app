949c006493b321bd375e983aecec6af536909323
over-geek <kenagy100@gmail.com>
2023-10-12 18:26:14 +0000

    Create API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,24 @@ Rails.application.routes.draw do
   devise_for :users
   # Adding nested resources routes for users and posts
   resources :users , only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show] do
+    resources :posts, only: [:index, :new, :create, :show, :destroy] do
       post 'addlike', on: :member
       delete 'deletelike', on: :member
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
     end
   end
 
   # Defines the root path route ("/")
   root "users#index"
+
+  # API endpoints
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
 end
