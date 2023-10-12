cd11017b759851dc8fbbc48c6eaca32ec99d248a
over-geek <kenagy100@gmail.com>
2023-10-12 18:10:52 +0000

    Fix rubocop issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,6 @@
 class Post < ApplicationRecord
   # association with other models: User, Comment and Like
   belongs_to :author, class_name: 'User', foreign_key: 'author_id_id'
-  has_many :comments, class_name: 'Comment', foreign_key: 'posts_id'
-  has_many :likes, class_name: 'Like', foreign_key: 'posts_id'
-
-  # custom query: the five most recent comments for a given post
-  def most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
-  # attributes validations
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-
-  # update the posts counter of a User
-  after_save :update_posts_counter_by_user
-  after_destroy :update_posts_counter_by_userclass Post < ApplicationRecord
-  # association with other models: User, Comment and Like
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'posts_id', dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'posts_id', dependent: :destroy
 
@@ -37,11 +18,6 @@ class Post < ApplicationRecord
   # update the posts counter of a User
   after_save :update_posts_counter_by_user
   after_destroy :update_posts_counter_by_user
-
-  def update_posts_counter_by_user
-    author.update(posts_counter: author.posts.count)
-  end
-end
 
   def update_posts_counter_by_user
     author.update(posts_counter: author.posts.count)
